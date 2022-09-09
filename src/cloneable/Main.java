3eaebc6b61b0c6283c697d86f29079fad6d135f7
Sakshi Gupta <sakshimittal199419@gmail.com>
2022-09-09 18:59:04 +0530

    Testing feature merge

--- src/cloneable/Main.java
+++ src/cloneable/Main.java
@@ -2,7 +2,7 @@ package cloneable;
 
 public class Main {
     public static void main(String args[]) throws CloneNotSupportedException {
-        System.out.println("Cloneable example");
+        System.out.println("Cloneable example 2");
         Department department=new Department(101,"CSE");
         Student student=new Student(1,"Sakshi",department);
         Student student1=(Student) student.clone();
